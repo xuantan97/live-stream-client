c85ab335ea5004f6360d7b42a1aee114aa523d6f
Tan <xuantan97@gmail.com>
2019-04-13 22:29:33 +0700

    init webrtc

---
+++ src/WebRTC.js
@@ -0,0 +1,23 @@
+import react from "react";
+import RTCMultiConnection from 'rtcmulticonnection';
+// Employee Component
+class Question extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            user: this.props.user,
+            token: '',
+        };
+        this.connection = new RTCMultiConnection();
+    }
+
+    a = "103.89.85.105:1234/";
+
+    componentDidMount() {
+        this.connection.socketURL = '103.89.85.105:1234/';
+    }
+    render() {
+    }
+}
+
+export default Question;
