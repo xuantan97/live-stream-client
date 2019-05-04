e03accf0727e64ab6ddc901a705d0b63c59c9fe6
anson190777 <anson1907@gmail.com>
2019-05-04 18:27:25 +0700

    add Login Register

--- src/Login.js
+++ src/Login.js
@@ -25,6 +25,34 @@ class Login extends Component {
     onSubmit = () => {
         const errors = this.validate(this.state.data);
         this.setState({errors});
+
+        var FormData = require('form-data');
+        var account = new FormData();
+        account.append('email',this.state.data.email);
+        account.append('password',this.state.data.password);
+        console.log(account);
+        fetch('http://bonddemo.tk/v1/system/login',{
+            method: 'POST',
+            headers: {},
+            // body: JSON.stringify({
+            //     email: this.state.data.email,
+            //     password: this.state.data.password
+            // })
+            body: account
+        })
+            .then(res => {
+                res.json().then(response => {
+                    console.log(response);
+                    if(response === "fail"){
+                        alert("Login fail !!!");
+                    }else{
+                        localStorage.setItem('token',response);
+                        this.props.history.push('/homepage');
+                    }
+                })
+            })
+            .catch(error => {console.log(error);});
+        // this.props.history.push('/homepage');
     };
     render() {
         const {data,errors} = this.state;
@@ -49,18 +77,18 @@ class Login extends Component {
                         <h1>
                             Login
                         </h1>
-                        <Form.Group controlId="formBasicEmail">
+                        <Form.Group>
                             <Form.Label>Email address</Form.Label>
                             <Form.Control type="email" id="email" name="email" placeholder="Enter email" value={data.email} onChange={this.onChange}/>
                             {errors.email && <span style={{color: '#ae5856'}}>{errors.email}</span>}
                         </Form.Group>
 
-                        <Form.Group controlId="formBasicPassword">
+                        <Form.Group>
                             <Form.Label>Password</Form.Label>
                             <Form.Control type="password" id="password" name="password" placeholder="Password" value={data.password} onChange={this.onChange}/>
                             {errors.password && <span style={{color: '#ae5856'}}>{errors.password}</span>}
                         </Form.Group>
-                        <Button variant="primary" type="submit">
+                        <Button variant="primary" onClick={()=>this.onSubmit()}>
                             Submit
                         </Button>
                         <Button variant="primary" href="/register">
--- src/Register.js
+++ src/Register.js
@@ -5,6 +5,7 @@ import Validator from 'validator';
 class Register extends Component {
     state = {
         data: {
+            username: "",
             email: "",
             password: "",
             passwordConfirm: "",
@@ -14,9 +15,10 @@ class Register extends Component {
 
     validate = data => {
         const errors = {};
+        if (!data.username) errors.username = "Can't be blank";
         if (!Validator.isEmail(data.email)) errors.email = "Invalid Email";
         if (!data.password) errors.password = "Can't be blank";
-        if (!data.passwordConfirm !== data.password && data.passwordConfirm === "") errors.passwordConfirm = "Invalid Password";
+        if (data.passwordConfirm !== data.password && data.passwordConfirm === "") errors.passwordConfirm = "Invalid Password";
         return errors;
     };
 
@@ -27,6 +29,35 @@ class Register extends Component {
     onSubmit = () => {
         const errors = this.validate(this.state.data);
         this.setState({errors});
+
+        // console.log(this.state.data);
+        var FormData = require('form-data');
+        var account = new FormData();
+        account.append('username',this.state.data.username);
+        account.append('email',this.state.data.email);
+        account.append('password',this.state.data.password);
+        console.log(account);
+        fetch('http://bonddemo.tk/v1/system/sign-up',{
+            method: 'POST',
+            headers: {},
+            // body: JSON.stringify({
+            //     email: this.state.data.email,
+            //     password: this.state.data.password
+            // })
+            body: account
+        })
+            .then(res => {
+                res.json().then(response => {
+                    console.log(response);
+                    if(response === "fail"){
+                        alert("Regist failed !!!");
+                    }else{
+                        // localStorage.setItem('token',response);
+                        this.props.history.push('/');
+                    }
+                })
+            })
+            .catch(error => {console.log(error)});
     };
     render() {
         const {data,errors} = this.state;
@@ -51,25 +82,32 @@ class Register extends Component {
                         <h1>
                             Register
                         </h1>
-                        <Form.Group controlId="formBasicEmail">
+
+                        <Form.Group>
+                            <Form.Label>Username</Form.Label>
+                            <Form.Control type="text" id="username" name="username" placeholder="Enter username" value={data.username} onChange={this.onChange}/>
+                            {errors.username && <span style={{color: '#ae5856'}}>{errors.username}</span>}
+                        </Form.Group>
+
+                        <Form.Group>
                             <Form.Label>Email address</Form.Label>
                             <Form.Control type="email" id="email" name="email" placeholder="Enter email" value={data.email} onChange={this.onChange}/>
                             {errors.email && <span style={{color: '#ae5856'}}>{errors.email}</span>}
                         </Form.Group>
 
-                        <Form.Group controlId="formBasicPassword">
+                        <Form.Group>
                             <Form.Label>Password</Form.Label>
                             <Form.Control type="password" id="password" name="password" placeholder="Password" value={data.password} onChange={this.onChange}/>
                             {errors.password && <span style={{color: '#ae5856'}}>{errors.password}</span>}
                         </Form.Group>
 
-                        <Form.Group controlId="formBasicPassword2">
+                        <Form.Group>
                             <Form.Label>Password Confirmation</Form.Label>
                             <Form.Control type="password" id="passwordConfirm" name="passwordConfirm" placeholder="Password" value={data.passwordConfirm} onChange={this.onChange}/>
                             {errors.passwordConfirm && <span style={{color: '#ae5856'}}>{errors.passwordConfirm}</span>}
                         </Form.Group>
 
-                        <Button variant="primary" type="submit">
+                        <Button variant="primary" onClick={()=>this.onSubmit()}>
                             Submit
                         </Button>
                     </Form>
