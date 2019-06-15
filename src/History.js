1a01cce33a81992e65de4c420871a6a54e49f5da
Phan Văn Tấn <tanphan0805@gmail.com>
2019-06-16 01:11:45 +0700

    update history

--- src/History.js
+++ src/History.js
@@ -14,7 +14,8 @@ class History extends Component{
     this.state = {
       products: [],
       name_program: "",
-      created_at: ""
+      created_at: "",
+      objs: []
     };
   }
 
@@ -27,76 +28,91 @@ class History extends Component{
         })
         .then(res => res.json())
         .then(response => {
-            console.log(response);
+          console.log(response);
             this.setState({
-              products: response.user,
-              name_program: response.program.name_program,
-              created_at: response.program.created_at
+             objs: response,
             });
         })
         .catch(error => console.log(error));
   }
 
-  render(){
-    const columns = [{
-      dataField: 'username',
-      text: 'Tên'
-    }, {
-      dataField: 'email',
-      text: 'Email'
-    }, {
-      dataField: 'money',
-      text: 'Tiền thưởng'
-    }];
+  getDataTable(objs) {
+    const columns = [
+      {
+        dataField: 'username',
+        text: 'Tên'
+      }, 
+      {
+        dataField: 'email',
+        text: 'Email'
+      }, 
+      {
+        dataField: 'money',
+        text: 'Tiền thưởng'
+      }
+    ];
 
+    var result = [];
+    objs.map((obj)=> {
+      result.push(
+        <div style={{backgroundColor: '#528B8B', marginTop: 1, color: '#000'}}>
+          <Collapsible trigger={ obj.program.name_program } >
+              <BootstrapTable keyField='id' data={ obj.user } columns={ columns } />
+          </Collapsible>
+        </div>
+      );
+    })
+    return result;
+  }
+
+
+  render(){
     return(
       <div className="site-wrap">
          <div className="site-mobile-menu site-navbar-target">
-    <div className="site-mobile-menu-header">
-      <div className="site-mobile-menu-close mt-3">
-        <span><FaTimes className="icon-close2 js-menu-toggle"/></span>
-      </div>
-    </div>
-    <div className="site-mobile-menu-body" />
-  </div>
-  <div id="sticky-wrapper" className="sticky-wrapper">
-  <header className="site-navbar py-4 js-sticky-header site-navbar-target" role="banner"
-      style={{width: '100%', position: 'fixed', top: '0px', transition: '0.5s'}}>
-    <div className="container">
-      <div className="row align-items-center">
-        <div className="col-6 col-xl-2">
-          <h1 className="mb-0 site-logo"><Link className="h2 mb-0" to="/homepage">Trivia<span>Game</span></Link></h1>
+          <div className="site-mobile-menu-header">
+            <div className="site-mobile-menu-close mt-3">
+              <span><FaTimes className="icon-close2 js-menu-toggle"/></span>
+            </div>
+          </div>
+          <div className="site-mobile-menu-body" />
         </div>
-        <div className="col-12 col-md-10 d-none d-xl-block">
-          <nav className="site-navigation position-relative text-right" role="navigation">
-            <ul className="site-menu main-menu js-clone-nav mr-auto d-none d-lg-block">
-              <li><Link to="/homepage">Trang chủ</Link></li>
-              <li><Link to="/aboutus">Chúng tôi</Link></li>
-              <li><Link to="/game">Trò chơi</Link></li>
-              <li><Link to="/contact" className="site-menu-focus contact">Liên hệ</Link></li>
-              <li><Link to="/history">Lịch sử</Link></li>
-              <li>
-                <NavDropdown title={<FaUserAlt style={{ fontSize: '16px', marginBottom: '5px' }} />} id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#" onClick={()=>this.props.history.push('/profile')}>Profile</NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#" onClick={() => this.logout()}>Log out</NavDropdown.Item>
-                </NavDropdown>
-                </li>
-            </ul>
-          </nav>
+        <div id="sticky-wrapper" className="sticky-wrapper">
+        <header className="site-navbar py-4 js-sticky-header site-navbar-target" role="banner"
+            style={{width: '100%', position: 'fixed', top: '0px', transition: '0.5s'}}>
+          <div className="container">
+            <div className="row align-items-center">
+              <div className="col-6 col-xl-2">
+                <h1 className="mb-0 site-logo"><Link className="h2 mb-0" to="/homepage">Trivia<span>Game</span></Link></h1>
+              </div>
+              <div className="col-12 col-md-10 d-none d-xl-block">
+                <nav className="site-navigation position-relative text-right" role="navigation">
+                  <ul className="site-menu main-menu js-clone-nav mr-auto d-none d-lg-block">
+                    <li><Link to="/homepage">Trang chủ</Link></li>
+                    <li><Link to="/aboutus">Chúng tôi</Link></li>
+                    <li><Link to="/game">Trò chơi</Link></li>
+                    <li><Link to="/contact" className="site-menu-focus contact">Liên hệ</Link></li>
+                    <li><Link to="/history">Lịch sử</Link></li>
+                    <li>
+                      <NavDropdown title={<FaUserAlt style={{ fontSize: '16px', marginBottom: '5px' }} />} id="basic-nav-dropdown">
+                        <NavDropdown.Item href="#" onClick={()=>this.props.history.push('/profile')}>Profile</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#" onClick={() => this.logout()}>Log out</NavDropdown.Item>
+                      </NavDropdown>
+                      </li>
+                  </ul>
+                </nav>
+              </div>
+              <div className="col-6 d-inline-block d-xl-none ml-md-0 py-3" style={{position: 'relative', top: '3px'}}><a href="#" className="site-menu-toggle js-menu-toggle text-black float-right"><span><FaList className="icon-menu h3"/></span></a></div>
+            </div>
+          </div>
+        </header>
         </div>
-        <div className="col-6 d-inline-block d-xl-none ml-md-0 py-3" style={{position: 'relative', top: '3px'}}><a href="#" className="site-menu-toggle js-menu-toggle text-black float-right"><span><FaList className="icon-menu h3"/></span></a></div>
-      </div>
-    </div>
-  </header>
-  </div>
         <section className="site-section bg-light" id="contact-section" style={{marginTop: '69px'}}>
           <div className="container">
             <div className="row mb-5">
               <div className="col-12 text-center">
-              <Collapsible trigger={ this.state.name_program }>
-                <BootstrapTable keyField='id' data={ this.state.products } columns={ columns } />
-              </Collapsible>
+                  {this.getDataTable(this.state.objs)}
               </div>
             </div>
           </div>
