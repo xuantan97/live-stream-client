b83e6ed3e0bcf309aa34286e3a69f4cf9031e098
Phan Văn Tấn <tanphan0805@gmail.com>
2019-06-16 23:05:53 +0700

    update Login

--- src/Login.js
+++ src/Login.js
@@ -30,33 +30,30 @@ class Login extends Component {
         var account = new FormData();
         account.append('email',this.state.data.email);
         account.append('password',this.state.data.password);
-        console.log(account);
+
         fetch('http://bonddemo.tk/v1/system/login',{
             method: 'POST',
             headers: {},
-            // body: JSON.stringify({
-            //     email: this.state.data.email,
-            //     password: this.state.data.password
-            // })
             body: account
         })
-            .then(res => {
-                res.json().then(response => {
-                    console.log(response);
-                    if(response === "fail"){
-                        alert("Đăng nhập không thành công !!!");
-                    }else{
-                        localStorage.setItem('user_id', response.id);
-                        localStorage.setItem('email', response.email);
-                        localStorage.setItem('username', response.username);
-                        localStorage.setItem('token', response.token);
-                        this.props.history.push('/homepage');
-                    }
-                })
+        .then(res => {
+            res.json().then(response => {
+                console.log(response);
+                if(response === "OK"){
+                    localStorage.setItem('user_id', response.id);
+                    localStorage.setItem('email', response.email);
+                    localStorage.setItem('username', response.username);
+                    localStorage.setItem('token', response.token);
+                    this.props.history.push('/homepage');
+                }else{
+                    alert("Đăng nhập không thành công !!!");
+                }
             })
-            .catch(error => {console.log(error);});
-        // this.props.history.push('/homepage');
+        })
+        .catch(error => {console.log(error);});
     };
+
+
     render() {
         const {data,errors} = this.state;
         let email = '';
@@ -95,35 +92,6 @@ class Login extends Component {
                         <li></li> <li></li> <li></li> <li></li> <li></li>
                     </ul>
                 </div>
-
-
-                {/* <div className="container">
-
-                    <Form onSubmit={this.onSubmit}>
-                        <h1>
-                            Login
-                        </h1>
-                        <Form.Group>
-                            <Form.Label>Email address</Form.Label>
-                            {errors.email && <span style={{color: '#ae5856', float: 'right'}}>{errors.email}</span>}
-                            <Form.Control className={email} type="email" id="email" name="email" placeholder="abc@gmail.com" value={data.email} onChange={this.onChange}/>
-                        </Form.Group>
-
-                        <Form.Group>
-                            <Form.Label>Password</Form.Label>
-                            {errors.password && <span style={{color: '#ae5856', float: 'right'}}>{errors.password}</span>}
-                            <Form.Control className={password} type="password" id="password" name="password" placeholder="123456" value={data.password} onChange={this.onChange}/>
-                        </Form.Group>
-                        <Button className="submit" variant="primary" onClick={()=>this.onSubmit()}>
-                            Login
-                        </Button>
-                        <br/>
-                        <span style={{userSelect:"none"}}>Don't have account?&nbsp;</span>
-                        <a variant="primary" href="/register">
-                            Register
-                        </a>
-                    </Form>
-                </div> */}
             </div>
         )
     }
