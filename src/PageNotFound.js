66383bf43db9c2fdef9e1a1e61989d48fb52f120
DESKTOP-EP4014P\Dell <nttam199@gmail.com>
2019-06-20 17:53:32 +0700

    CSS for Page not found

--- src/PageNotFound.js
+++ src/PageNotFound.js
@@ -5,7 +5,7 @@ class PageNotFound extends Component {
 
     render() {            
         return(
-            <div className="abc">
+            <div className="page-not-found">
                 <div className="title-page-not-found">404!</div>     
                 <p className="content-page-not-found">Trang bạn đang tìm kiếm không tồn tại</p>
                 <button>Quay lại</button>
