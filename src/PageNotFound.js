c0234420b77239b1edac3ce9941b019f0cfca675
DESKTOP-EP4014P\Dell <nttam199@gmail.com>
2019-06-20 17:42:50 +0700

    CSS for Page not found

--- src/PageNotFound.js
+++ src/PageNotFound.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
+import $ from "jquery";
 
 class PageNotFound extends Component {
 
-    render() {
+    render() {            
         return(
-            <div >
-                <span>Page Not Found</span>                
+            <div className="abc">
+                <div className="title-page-not-found">404!</div>     
+                <p className="content-page-not-found">Trang bạn đang tìm kiếm không tồn tại</p>
+                <button>Quay lại</button>
             </div>
         )
     }
