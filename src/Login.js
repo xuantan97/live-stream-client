d8d576d0b3f760c5458dba65b8dcff3936df7200
Phan Văn Tấn <tanphan0805@gmail.com>
2019-06-25 00:06:23 +0700

    validate Login, Register

--- src/Login.js
+++ src/Login.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import {Button} from 'react-bootstrap';
 import { Link } from "react-router-dom";
-import SnackBar from 'react-material-snackbar';
+import Validator from 'validator';
+import $ from "jquery";
 
 class Login extends Component {
     state = {
@@ -9,8 +10,15 @@ class Login extends Component {
             email: "",
             password: "",
         },
-        showSnackBar: false,
-        messageError: ""
+        errors: {},
+        showLoginFail: false,
+    };
+
+    validate = data => {
+        const errors = {};
+        if (!Validator.isEmail(data.email)) errors.email = "Email không hợp lệ";
+        if (data.password.length < 6 || data.password.length > 16) errors.password = "Mật khẩu chỉ được nhập từ 6-16 ký tự";
+        return errors;
     };
    
 
@@ -19,64 +27,68 @@ class Login extends Component {
     });
 
 
-    onSubmit = () => {
-        if(this.state.data.email === "" || this.state.data.password === "") {
-            this.setState({
-                showSnackBar: true,
-                messageError: "Form đăng nhập không được để trống"
-            });
-        }
-        else {
-            if(this.state.data.password.length < 6 || this.state.data.password.length > 16) {
-                this.setState({
-                    showSnackBar: true,
-                    messageError: "Password chỉ được nhập từ 6-16 ký tự"
-                });
-            }
-            else {
-                var account = new FormData();
-                account.append('email', this.state.data.email);
-                account.append('password', this.state.data.password);
+    onSubmit = async () => {
+        const errors = await this.validate(this.state.data);
+        await this.setState({
+            errors,
+            showLoginFail: false
+        });
 
-                fetch('http://bonddemo.tk/v1/system/login',{
-                    method: 'POST',
-                    headers: {},
-                    body: account
+        if($.isEmptyObject(this.state.errors) === true) {
+            var account = new FormData();
+            account.append('email', this.state.data.email);
+            account.append('password', this.state.data.password);
+
+            fetch('http://bonddemo.tk/v1/system/login',{
+                method: 'POST',
+                headers: {},
+                body: account
+            })
+            .then(res => {
+                res.json().then(response => {
+                    console.log(response);
+                    if(response === "fail"){
+                        this.setState({
+                            showLoginFail: true,
+                        });
+                    }else{
+                        localStorage.setItem('user_id', response.id);
+                        localStorage.setItem('email', response.email);
+                        localStorage.setItem('username', response.username);
+                        localStorage.setItem('token', response.token);
+                        localStorage.setItem('loggedIn', true);
+                        this.props.history.push('/homepage');
+                    }
                 })
-                .then(res => {
-                    res.json().then(response => {
-                        console.log(response);
-                        if(response === "fail"){
-                            this.setState({
-                                showSnackBar: true,
-                                messageError: "Đăng nhập không thành công !!!"
-                            });
-                        }else{
-                            localStorage.setItem('user_id', response.id);
-                            localStorage.setItem('email', response.email);
-                            localStorage.setItem('username', response.username);
-                            localStorage.setItem('token', response.token);
-                            localStorage.setItem('loggedIn', true);
-                            this.props.history.push('/homepage');
-                        }
-                    })
-                })
-                .catch(error => {console.log(error);});
-            }
-            
+            })
+            .catch(error => {console.log(error);});
         }
     };
 
 
     render() {
+        const {data, errors} = this.state;
+        let email = '';
+        let password = '';
+        if(errors.email) {
+            email += 'error';
+        }
+        if(errors.password) {
+            password += 'error';
+        }
         return(
             <div className="container-form">
 
                 <div className="wrapper-login">
                     <div className="container-login">
                         <h1>Đăng nhập</h1>
+                        { this.state.showLoginFail ? <h4 style={{color: 'red'}}>Đăng nhập không thành công</h4> : null }
+                        
                         <form className="form-login" onSubmit={this.onSubmit}>
+                            {errors.email && <div style={{color: '#ae5856'}}>{errors.email}</div>}
                             <input type="email" id="email" name="email" placeholder="Email" value={this.state.data.email} onChange={this.onChange}/>
+                            
+                            {errors.password && <div style={{color: '#ae5856'}}>{errors.password}</div>}
                             <input type="password" id="password" name="password" placeholder="Mật khẩu" value={this.state.data.password} onChange={this.onChange}/>
                             <Button className="submit" variant="primary" onClick={()=>this.onSubmit()}>Đăng nhập</Button>
                             <div style={{marginTop: '30px', fontSize: '18px'}}>
@@ -89,14 +101,6 @@ class Login extends Component {
                         <li></li> <li></li> <li></li> <li></li> <li></li>
                         <li></li> <li></li> <li></li> <li></li> <li></li>
                     </ul>
-                    <div>
-                        <SnackBar
-                            show={this.state.showSnackBar}                            
-                            timer={3000}                           
-                            >
-                            <p>{this.state.messageError} </p>
-                        </SnackBar>
-                    </div>
                 </div>
             </div>
         )
--- src/Register.js
+++ src/Register.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import {Button} from 'react-bootstrap';
 import Validator from 'validator';
 import { Link } from "react-router-dom";
+import $ from "jquery";
 
 class Register extends Component {
     state = {
@@ -12,14 +13,15 @@ class Register extends Component {
             passwordConfirm: "",
         },
         errors: {},
+        showRegisterFail: false,
     };
 
     validate = data => {
         const errors = {};
         if (!data.username) errors.username = "Tên đăng nhập không được trống";
         if (!Validator.isEmail(data.email)) errors.email = "Email không hợp lệ";
-        if (!data.password) errors.password = "Mật khẩu không được trống";
-        if (data.passwordConfirm !== data.password || data.passwordConfirm === "") errors.passwordConfirm = "Mật khẩu không chính xác";
+        if (data.password.length < 6 || data.password.length > 16) errors.password = "Mật khẩu chỉ được nhập từ 6-16 ký tự";
+        if (data.passwordConfirm !== data.password) errors.passwordConfirm = "Mật khẩu xác nhận không chính xác";
         return errors;
     };
 
@@ -27,33 +29,42 @@ class Register extends Component {
         data: {...this.state.data, [e.target.name]: e.target.value}
     });
 
-    onSubmit = () => {
-        const errors = this.validate(this.state.data);
-        this.setState({errors});
+     onSubmit = async () => {
+        const errors = await this.validate(this.state.data);
+        await this.setState({
+            errors, 
+            showRegisterFail: false
+        });
 
-        var FormData = require('form-data');
-        var account = new FormData();
-        account.append('username',this.state.data.username);
-        account.append('email',this.state.data.email);
-        account.append('password',this.state.data.password);
-        console.log(account);
-        fetch('http://bonddemo.tk/v1/system/sign-up',{
-            method: 'POST',
-            headers: {},
-            body: account
-        })
+        console.log(this.state.errors);
+         if($.isEmptyObject(this.state.errors) === true) {
+            var account = new FormData();
+            account.append('username',this.state.data.username);
+            account.append('email',this.state.data.email);
+            account.append('password',this.state.data.password);
+            console.log(account);
+            fetch('http://bonddemo.tk/v1/system/sign-up',{
+                method: 'POST',
+                headers: {},
+                body: account
+            })
             .then(res => {
                 res.json().then(response => {
                     console.log(response);
                     if(response === "fail"){
-                        alert("Đăng kí không thành công !!!");
+                        this.setState({
+                            showRegisterFail: true,
+                        });
                     }else{
                         this.props.history.push('/');
                     }
                 })
             })
             .catch(error => {console.log(error)});
+        }
     };
+
+    
     render() {
         const {data,errors} = this.state;
         let userName = '';
@@ -76,11 +87,12 @@ class Register extends Component {
         return(
             <div className="container-form">
                 
-                <div class="wrapper-login">
-                    <div class="container-login">
+                <div className="wrapper-login">
+                    <div className="container-login">
                         <h1>Đăng kí</h1>
+                        { this.state.showRegisterFail ? <h4 style={{color: 'red'}}>Đăng ký không thành công</h4> : null }
                         
-                        <form class="form-login" onSubmit={this.onSubmit}>
+                        <form className="form-login" onSubmit={this.onSubmit}>
                             {errors.username && <span style={{color: '#ae5856', fontSize: '14px'}}>{errors.username}</span>}
                             <input className={userName} type="text" id="username" name="username" placeholder="Tên đăng nhập" value={data.username} onChange={this.onChange}/>
 
@@ -101,7 +113,7 @@ class Register extends Component {
                         </form>
                     </div>
                     
-                    <ul class="bg-bubbles">
+                    <ul className="bg-bubbles">
                         <li></li> <li></li> <li></li> <li></li> <li></li>
                         <li></li> <li></li> <li></li> <li></li> <li></li>
                     </ul>
